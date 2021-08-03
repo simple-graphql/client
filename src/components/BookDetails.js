5c50c3aee6964f7a6bdfda90f5c1d01d8e85dc55
Mohamed Tawfik <motawfik98@outlook.com>
2021-08-03 23:55:50 +0200

    use getBookQuery to display book data

--- src/components/BookDetails.js
+++ src/components/BookDetails.js
@@ -1,11 +1,37 @@
-import {useLazyQuery} from "@apollo/client";
+import {useQuery} from "@apollo/client";
 import {getBookQuery} from "../queries/queries";
 
-const BookDetails = () => {
-  const [getBook, {loading, error, data}] = useLazyQuery(getBookQuery);
+const BookDetails = (props) => {
+
+  const {loading, error, data} = useQuery(getBookQuery, {
+    variables: {
+      id: props.bookID
+    },
+    skip: !props.bookID
+  });
+
+  const displayBookDetails = () => {
+    if (!loading && props.bookID) {
+      const book = data.book
+      return (
+        <div>
+          <h2>{book.name}</h2>
+          <p>{book.genre}</p>
+          <p>{book.author.name}</p>
+          <p>All books by this author</p>
+          <ul className="other-books">
+            {book.author.books.map(book => {
+              return <li key={book.id}>{book.name}</li>
+            })}
+          </ul>
+        </div>
+      );
+    }
+    return (<div>No book selected</div>)
+  }
   return (
     <div id="book-details">
-      <p>Output book details here</p>
+      {displayBookDetails()}
     </div>
   );
 };
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,16 +1,18 @@
 import {getBooksQuery} from "../queries/queries";
 import {useQuery} from "@apollo/client";
 import BookDetails from "./BookDetails";
+import {useState} from "react";
 
 const BookList = () => {
   const {loading, error, data} = useQuery(getBooksQuery);
+  const [selectedBook, setSelectedBook] = useState();
   const displayBooks = () => {
     if (loading) return (<div>Loading Books...</div>)
     if (error) return (<div>Error loading books...</div>)
 
     return data.books.map(book => {
       return (
-        <li key={book.id}>{book.name}</li>
+        <li key={book.id} onClick={() => {setSelectedBook(book.id)}}>{book.name}</li>
       )
     })
   }
@@ -20,7 +22,7 @@ const BookList = () => {
       <ul id="book-list">
         {displayBooks()}
       </ul>
-      <BookDetails />
+      <BookDetails bookID={selectedBook}/>
     </div>
   );
 }
