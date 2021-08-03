bb3859ea2878bf2a780865d57bfd883ffcfbe80e
Mohamed Tawfik <motawfik98@outlook.com>
2021-08-03 20:18:28 +0200

    added empty BookList component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-function App() {
+// components
+import BookList from "./components/BookList";
+
+const App = () => {
   return (
     <div id="main">
       <h1>Hello There</h1>
+      <BookList />
     </div>
   );
 }
---
+++ src/components/BookList.js
@@ -0,0 +1,11 @@
+const BookList = () => {
+  return (
+    <div>
+      <ul id="book-list">
+        <li>Book Name</li>
+      </ul>
+    </div>
+  );
+}
+
+export default BookList;
